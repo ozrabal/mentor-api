683ccba9a9b007d42dc77333075637f0c5e5f0c0
Piotr Łepkowski <ozrabal@gmail.com>
2026-01-08 20:09:42 +0100

    feat: Add health module with health check endpoint to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, HealthModule],
 })
 export class AppModule {}
 
